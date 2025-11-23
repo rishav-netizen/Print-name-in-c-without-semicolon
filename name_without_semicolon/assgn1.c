963e6d295485f4abe6c7b2bac4013c471ee9018c
Rishav <77397031+Rishav-Kumar2007@users.noreply.github.com>
2025-11-24 01:45:40 +0530

    Add files via upload

---
+++ name_without_semicolon/assgn1.c
@@ -0,0 +1,10 @@
+//? Write your name in c without using a semicolon
+#include <stdio.h>
+
+int main()
+{
+    if (printf("Rishav\n")) // printf returns number of chars in string so its basically if(True)
+    {
+        return 0;
+    }
+}
